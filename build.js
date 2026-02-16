1cb748038a13af9366e3e9ae4a8a4b3dff897da6
Jeimy Rojas <rojaslopez.jeimy@gmail.com>
2026-02-15 18:59:59 -0800

    rename dist folder to docs

--- build.js
+++ build.js
@@ -1,22 +1,22 @@
-// cp ./src/public/* ./dist
-// cp ./src/main.js ./dist
-// cp ./src/index.html ./dist
+// cp ./src/public/* ./docs
+// cp ./src/main.js ./docs
+// cp ./src/index.html ./docs
 
 const fs = require('fs');
 
 // copy directory
-fs.cp('./src/public', './dist', { recursive: true }, (err) => {
+fs.cp('./src/public', './docs', { recursive: true }, (err) => {
   if (err) {
     console.error(err);
   }
 });
 
-fs.cp('./src/main.js', './dist/main.js', (err) => {
+fs.cp('./src/main.js', './docs/main.js', (err) => {
   if (err) {
     console.error(err);
   }
 });
-fs.cp('./src/index.html', './dist/index.html', (err) => {
+fs.cp('./src/index.html', './docs/index.html', (err) => {
   if (err) {
     console.error(err);
   }
