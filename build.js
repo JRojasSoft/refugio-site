db449100ba019c141426f600ce4b86e51834f1fc
Jeimy Rojas <rojaslopez.jeimy@gmail.com>
2026-02-22 19:06:42 -0800

    simplify build script image processing

--- build.js
+++ build.js
@@ -1,65 +1,53 @@
-const sharp = require('sharp');
 const fs = require('fs');
+const sharp = require('sharp');
+
 const src = 'src';
 const dest = 'docs';
 const imgSrc = `${src}/img`;
 const imgDest = `${dest}/img`;
 const cssDest = `${dest}/css`;
+const onError = (err) => err && console.error(err);
 
-console.log('Clearing dest files...\n');
-// Delete existing files
+console.log('Clearing dest folder...\n');
 fs.rmSync(`${dest}`, { recursive: true, force: true });
 
-console.log('Copying files...');
-// Copy root level src files
-['index.html' , '404.html' , 'js/main.js'].forEach((file) => {
-  console.log('Copy: ', file);
-  fs.cp(`${src}/${file}`, `${dest}/${file}`, (err) => err && console.error(err));
-});
+console.log('Copying static folders & files...');
+fs.cp(`${src}/404.html`, `${dest}/404.html`, onError);
+fs.cp(`${src}/index.html`, `${dest}/index.html`, onError);
+fs.cp(`${src}/js`, `${dest}/js`, {recursive: true}, onError);
 
 console.log('\nCreating images dest folder...\n');
-// Create img folder in docs
-fs.mkdirSync(`${dest}/img`, { recursive: true });
-fs.mkdirSync(`${dest}/brand`, { recursive: true });
+fs.mkdirSync(`${dest}/img/brand`, { recursive: true });
+
+function processImg(fileName, format, outFormat, formatOpts = {quality: 50}, resizeOpts = {}, blur = null) {
+  const sharpStream = sharp(`${imgSrc}/${fileName}.${format}`)
+    .resize(resizeOpts)
+    .toFormat(outFormat, formatOpts);
+  if (blur) {
+    sharpStream.blur(blur);
+  }
+  return sharpStream.toFile(`${imgDest}/${fileName}.${outFormat}`, onError);
+}
 
 console.log('Processing and optimizing images...\n');
-// Resize, process and optimize images
-sharp(`${src}/public/favicon.png`)
-  .png({ quality: 80 })
-  .toFile(`${dest}/brand/favicon.png`, (err) => err && console.error(err));
 
-sharp(`${imgSrc}/logo.png`)
-  .resize(271).webp({ quality: 50, lossless: true })
-  .toFile(`${imgDest}/logo.webp`, (err) => err && console.error(err));
-sharp(`${imgSrc}/logotype.png`)
-  .resize(340).webp({ quality: 50, lossless: true })
-  .toFile(`${imgDest}/logotype.webp`, (err) => err && console.error(err));
+processImg('brand/favicon', 'png', 'png', { quality: 80 }, 
+  { width: 48, height: 48, fit: 'contain', background: {r:0, g:0, b:0, alpha:0} });
 
-sharp(`${imgSrc}/placeholder-garden.jpg`).blur(3)
-  .resize(1920, 1080, { position: 'top' }).avif({ quality: 50 })
-  .toFile(`${imgDest}/placeholder-garden.avif`, (err) => err && console.error(err));
-sharp(`${imgSrc}/lots-blueprint-color.jpg`)
-  .resize(1650).avif({ quality: 50 })
-  .toFile(`${imgDest}/lots-blueprint-color.avif`, (err) => err && console.error(err));
-sharp(`${imgSrc}/splash-dark.jpg`)
-  .resize(1920).avif({ quality: 80 })
-  .toFile(`${imgDest}/splash-dark.avif`, (err) => err && console.error(err));
+processImg('brand/logo', 'png', 'webp', { quality: 50, lossless: true }, { width: 271 });
+processImg('brand/logotype', 'png', 'webp', { quality: 50, lossless: true }, { width: 340 });
 
-sharp(`${imgSrc}/render-home.jpg`)
-  .resize(null, 700).avif({ quality: 50 })
-  .toFile(`${imgDest}/render-home.avif`, (err) => err && console.error(err));
-['green-areas', 'render-project-entrance'].forEach((file) => {
-  sharp(`${imgSrc}/${file}.png`)
-    .resize(null, 700).avif({ quality: 50 })
-    .toFile(`${imgDest}/${file}.avif`, (err) => err && console.error(err));
-});
+processImg('splash-dark', 'jpg', 'avif', { quality: 80 }, {width: 1920});
+processImg('placeholder-garden', 'jpg', 'avif', { quality: 50 }, {width: 1920, height: 1080, position: 'top'}, 3);
+processImg('lots-blueprint-color', 'jpg', 'avif', { quality: 50 }, {width: 1650});
+
+processImg('render-home', 'jpg', 'avif', { quality: 50 }, {height: 700});
+processImg('green-areas', 'png', 'avif', { quality: 50 }, {height: 700});
+processImg('render-project-entrance', 'png', 'avif', { quality: 50 }, {height: 700});
 
 ['street', 'street-sign', 'transmission-tower', 'water-tap'].forEach((file) => {
-  sharp(`${imgSrc}/${file}.png`)
-    .webp({ quality: 50, lossless: true })
-    .toFile(`${imgDest}/${file}.webp`, (err) => err && console.error(err));
+  processImg(file, 'png', 'webp', { quality: 50, lossless: true });
 });
   
 console.log('Creating styles dest folder...');
-// Create css folder in docs
-fs.mkdir(cssDest, { recursive: true }, (err) => err && console.error(err));
+fs.mkdir(cssDest, { recursive: true }, onError);
